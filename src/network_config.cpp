7de989dd301502e30765eee3ed3a3f86ce0a7516
“enderkc” <enderhkc@gmail.com>
2026-06-26 18:52:04 +0800

    ci: add firmware release workflow

--- src/network_config.cpp
+++ src/network_config.cpp
@@ -1,13 +1,13 @@
 #include "network_config.h"
 
-const char *mqtt_broker = "x93656e1.ala.cn-hangzhou.emqxsl.cn";
+const char *mqtt_broker = "";
 const char *mqtt_state_topic = "emqx/esp8266/state";
 const char *mqtt_command_topic = "emqx/esp8266/cmd";
-const char *mqtt_username = "EnderKC";
-const char *mqtt_password = "QWEqwe123";
+const char *mqtt_username = "";
+const char *mqtt_password = "";
 
-String weather_APIkey = "S6HxLK_QBsQ8jY3Rv";
-String driver_api_key = "SuBkUBbhV4zhLGD8R";
+String weather_APIkey = "";
+String driver_api_key = "";
 
 String weather_api = "http://api.seniverse.com/v3/weather/now.json?key=" +
                      weather_APIkey +
