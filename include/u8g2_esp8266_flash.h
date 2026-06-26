1be9f9fd64d2ebbc33ce798cc583e018049718f5
“enderkc” <enderhkc@gmail.com>
2026-06-26 19:27:00 +0800

    fix: read u8g2 fonts from flash on esp8266

--- include/u8g2_esp8266_flash.h
+++ include/u8g2_esp8266_flash.h
@@ -5,4 +5,9 @@
 #define U8X8_FONT_SECTION(name) __attribute__((section(".irom.text." name)))
 #endif
 
+#if defined(ESP8266) && !defined(u8x8_pgm_read)
+#include <pgmspace.h>
+#define u8x8_pgm_read(adr) pgm_read_byte(adr)
+#endif
+
 #endif // U8G2_ESP8266_FLASH_H
